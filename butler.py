757c15d34bee1a5985d63d391a2be3766d79c112
S8s8Max <k6play7210@gmail.com>
2021-05-10 18:23:23 +0900

    add butler.py

--- butler.py
+++ butler.py
@@ -1,3 +1,17 @@
-import slack_api_token
+from slack_api_token import SLACK_BOT_USER_OAUTH_TOKEN, SLACK_USER_OAUTH_TOKEN
+import requests
+import sys
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
+def SendMessage(text: str):
+    client = WebClient(token=SLACK_BOT_USER_OAUTH_TOKEN)
+    try:
+        response = client.chat_postMessage(channel='#times_sober-wizard', text=text)
+    except SlackApiError as e:
+        print("Got an Error : ", e)
 
+if __name__ == "__main__":
+    text = sys.argv[1]
+
+    SendMessage(text)
